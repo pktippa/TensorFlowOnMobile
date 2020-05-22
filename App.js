9b22ec4a3a4fa70d5eaf5fa5ebb04c2691cbf22a
Pradeep Kumar Tippa <pktipp@gmail.com>
2020-05-22 15:15:08 +0530

    Updated code with showing tile on home page.

--- App.js
+++ App.js
@@ -24,47 +24,41 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
+  const realTimePersonRemoval = () => {
+    console.log('clicked real time person removal');
+  };
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
+          {/* TODO can we reuse the similar header implementation
+            to show my app content */}
+          {/* <Header /> */}
+          {/* {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
-          )}
+          )} */}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+              <Text style={styles.sectionTitle} onPress={realTimePersonRemoval}>
+                Real Time Person Removal
+              </Text>
+              <Text
+                style={styles.sectionDescription}
+                onPress={realTimePersonRemoval}>
+                Real Time Person Removal
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
+            {/* <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+            </View> */}
           </View>
         </ScrollView>
       </SafeAreaView>
---
+++ README.md
@@ -0,0 +1,15 @@
+# TensorFlow On Mobile
+
+Applications built on top of TensorFlowJS using React Native.
+
+All the demos are listed as tiles vertically.
+
+Reference repos.
+
+Tensorflow TfJs React native integration_59
+
+## Real Time Person removal
+
+- Opens up permission for camera access.
+- Upon accept permission, opens camera with space using 50% of camera.
+- Render the same camera view on the second split view of remaining 50%.
